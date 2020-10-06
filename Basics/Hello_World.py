72e7b0238735b63a31d95bbedcc3f676b0b498e5
Santhosh Powell <santhosh.powell6@gmail.com>
2020-10-06 21:09:03 +0530

    Add files via upload

---
+++ Basics/Hello_World.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 13:57:21 2020
+
+@author: Santhosh
+"""
+
+print ("Hello World") #Normal print
+
+print("Can I say the \"String\"") #Print with double " in it
